8488c5f6b666443cbe0a7bff790d4ca0bbb7b02c
Loli <lolarlew@gmail.com>
2023-09-07 15:21:04 -0300

    avances

--- DesafioRelaciones/src/Servicio/ServicioTienda.java
+++ DesafioRelaciones/src/Servicio/ServicioTienda.java
@@ -11,7 +11,6 @@ import Entidades.Tienda;
 public class ServicioTienda {
 
 	public ArrayList<Tienda> listStores = new ArrayList<>();
-	// HashMap<Producto, Integer> carritoCliente = new HashMap<>();
 	ArrayList<Producto> carritoCliente = new ArrayList<>();
 
 	Scanner leer = new Scanner(System.in);
@@ -38,22 +37,19 @@ public class ServicioTienda {
 
 	public void crearTienda() {
 
-		Tienda store = new Tienda(1, "carrera 30 con calle 15", "Stephany");
+		Tienda store1 = new Tienda(1, "carrera 30 con calle 15", "Stephany");
 		Tienda store2 = new Tienda(2, "carrera 10 con calle 10", "Lola");
 
-		listStores.add(store);
+		listStores.add(store1);
 		listStores.add(store2);
 
 	}
 
 	public void mostrarProductos(Tienda tienda) {
-
 		tienda.getListaProductos().forEach(System.out::println);
-
 	}
 
 	public void mostrarTiendas() {
-
 		System.out.println("--Menu seleccionar tienda---");
 
 		System.out.println("1. Ver lista de tiendas---");
@@ -65,22 +61,19 @@ public class ServicioTienda {
 			break;
 		case 2:
 			leer.nextLine();
-			System.out.println("Ingresa la direccion");
-			String respuesta = leer.nextLine();
+			System.out.print("Ingresa el ID de la tienda: ");
+			int respuesta = leer.nextInt();
 
-			for (Tienda tienda2 : listStores) {
-				if (tienda2.getDireccion().equalsIgnoreCase(respuesta)) {
-					System.out.println(tienda2);
+			for (Tienda tienda : listStores) {
+				if (tienda.getId() == respuesta) {
+					System.out.println("Tienda: " + tienda);
 				}
 			}
-
 			break;
-
-		default:
-			break;
+		default:	break;
 		}
-
 	}
+
 
 	public void seleccionarTiendaPorId() {
 		System.out.println("Ingresa el id de la tienda");
@@ -92,41 +85,40 @@ public class ServicioTienda {
 			}
 		}
 	}
+	
 
 	public Producto buscarProducto() {
-
 		Producto product = new Producto();
 
-		System.out.println("Ingresa el nombre del producto");
+		System.out.print("Ingresa el nombre del producto que busca: ");
 		String nombre = leer.nextLine();
 
 		for (Producto producto : tiendaSeleccionada.getListaProductos()) {
-			if (producto.getNombre().equalsIgnoreCase(nombre))
-				;
-			return product;
+			if (producto.getNombre().equalsIgnoreCase(nombre)) {
+				System.out.println("Producto: " + producto);
+				return product;
+			}
 		}
-
 		return product;
 	}
 
 	public void modificarTienda() {
-
 		if (tiendaSeleccionada != null) {
-			System.out.println("�Qu� atributo quieres modificar ?");
-			System.out.println("1: Direccion");
-			System.out.println("2: Representante");
-			System.out.println("3: Productos");
+			System.out.println("Qué atributo quieres modificar? ");
+			System.out.println("1: Direccion.");
+			System.out.println("2: Representante.");
+			System.out.println("3: Productos.");
 			int opcion = leer.nextInt();
 
 			switch (opcion) {
 			case 1:
 				leer.nextLine();
-				System.out.println("Ingresa la nueva dirreccion");
+				System.out.print("Ingresa la nueva dirreccion: ");
 				tiendaSeleccionada.setDireccion(leer.nextLine());
 				break;
 			case 2:
 				leer.nextLine();
-				System.out.println("Ingresa el nombre del representante");
+				System.out.print("Ingresa el nombre del representante: ");
 				tiendaSeleccionada.setRepresentante(leer.nextLine());
 				break;
 			case 3:
@@ -134,82 +126,71 @@ public class ServicioTienda {
 				break;
 
 			default:
-				System.out.println("No seleccionaste una opci�n v�lida");
+				System.out.println("No seleccionaste una opción válida");
 				break;
 			}
-
 		} else {
-			System.out.println("No existe un producto con ese nombre ");
+			System.out.println("No existe un producto con ese nombre.");
 		}
 	}
 
 	public void modificarProducto() {
-
 		Producto product = buscarProducto();
 
-		// product != null
-
 		if (product.getNombre() != null) {
-			System.out.println("�Qu� atributo quieres modificar ?");
-			System.out.println("1: Nombre");
-			System.out.println("2: Precio");
-			System.out.println("3: Stock");
+			System.out.println("Qué atributo quieres modificar? ");
+			System.out.println("1: Nombre.");
+			System.out.println("2: Precio.");
+			System.out.println("3: Stock.");
 			int opcion = leer.nextInt();
 
 			switch (opcion) {
 			case 1:
 				leer.nextLine();
-				System.out.println("Ingresa el nuevo nombre");
+				System.out.print("Ingresa el nuevo nombre: ");
 				product.setNombre(leer.next());
 				break;
 			case 2:
-				System.out.println("Ingresa el nuevo precio");
+				System.out.print("Ingresa el nuevo precio: ");
 				product.setPrecio(leer.nextInt());
 				break;
 			case 3:
-
-				System.out.println("Ingresa la cantidad del producto");
+				System.out.print("Ingresa la cantidad del producto: ");
 				product.setStockProducto(leer.nextInt());
 				break;
-
 			default:
-				System.out.println("No seleccionaste una opci�n v�lida");
+				System.out.println("No seleccionaste una opción válida.");
 				break;
 			}
-
 		} else {
-			System.out.println("No existe un producto con ese nombre ");
+			System.out.println("No existe un producto con ese nombre.");
 		}
 	}
 
+
 	public void eliminarProducto() {
-
 		Producto product = buscarProducto();
-
 		tiendaSeleccionada.getListaProductos().remove(product);
-
 	}
 
-	public void eliminarTienda() {
 
+	public void eliminarTienda() {
+		seleccionarTiendaPorId();
 		listStores.remove(tiendaSeleccionada);
 	}
 
-	public void agregarProductosCarrito() {
 
+	public void agregarProductosCarrito() {
 		Producto product = buscarProducto();
 
 		System.out.println("Ingresa la cantidad que deseas comprar");
 		int cant = leer.nextInt();
 
 		product.setStockProducto(cant);
-
 		carritoCliente.add(product);
-
 	}
 
 	public void venderProductos() {
-
 		ArrayList<Producto> productosLista = tiendaSeleccionada.getListaProductos();
 
 		for (int i = 0; i < productosLista.size(); i++) {
@@ -220,7 +201,7 @@ public class ServicioTienda {
 			}
 		}
 
-		System.out.println("su compra a sido exitosa ");
+		System.out.println("su compra a sido exitosa.");
 
 //		ArrayList<Producto> productosLista= tiendaSeleccionada.getListaProductos();
 //		
